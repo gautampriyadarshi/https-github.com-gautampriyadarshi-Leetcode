0a90365f705459bdda4fef8646ba2c1eaed878c0
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2024-08-22 10:27:21 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.14%) - LeetHub

---
+++ 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
@@ -0,0 +1,51 @@
+class Solution {
+    string to_binary(int num)
+    {
+        int x = num;
+        string binary = "";
+
+        while(x > 0)
+        {
+            binary += to_string(x % 2);
+            x = x/2; 
+        }
+        reverse(binary.begin(), binary.end());
+        return binary;
+    }
+
+    string compliment(string &binary)
+    {
+        for(int i = 0; i < binary.size(); i++)
+        {
+            if(binary[i] == '0')
+                binary[i] = '1';
+            else
+                binary[i] = '0';
+        }
+        return binary;
+    }
+
+    int complement(string comp)
+    {
+        long long int ans = 0, p = 1;
+        int i = comp.size()-1;
+        while(i >= 0)
+        {
+            long long int x = (comp[i] - '0');
+            ans += (p * x);
+            i--;
+            p *= 2;
+        }
+        return ans;
+    }
+public:
+    int bitwiseComplement(int num) 
+    {
+        if(num == 0)
+            return 1;
+            
+        string binary = to_binary(num);
+        string comp = compliment(binary);
+        return complement(comp);
+    }
+};
