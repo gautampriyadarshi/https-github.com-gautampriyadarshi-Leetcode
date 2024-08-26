89147246976c06880e830a61ec99a7e324e6ea5d
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2024-08-26 12:58:23 +0530

    Time: 1336 ms (26.2%), Space: 0B (100%) - LeetHub

---
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -0,0 +1,18 @@
+(
+    SELECT u.name AS results
+    FROM MovieRating AS mr
+    JOIN Users AS u
+    ON u.user_id = mr.user_id
+    GROUP BY u.name
+    ORDER BY COUNT(*) DESC, u.name ASC LIMIT 1
+)
+UNION ALL
+(
+    SELECT m.title AS results
+    FROM MovieRating AS mr
+    JOIN Movies AS m
+    ON m.movie_id = mr.movie_id
+    WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+    GROUP BY m.title
+    ORDER BY AVG(mr.rating) DESC, m.title ASC LIMIT 1
+);
