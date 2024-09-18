80cfb501c954568b0eb08f28160d6a3bf60e408f
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2024-09-18 09:45:08 +0530

    Time: 15 ms (6.24%), Space: 15.6 MB (85.32%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    static bool cmp(int x , int y) 
+    {
+        string xy = to_string(x) + to_string(y);
+        string yx = to_string(y) + to_string(x);
+        return xy > yx;
+    }
+public:
+    string largestNumber(vector<int>& nums) 
+    {
+        sort(nums.begin(), nums.end(), cmp);
+        string ans = "";
+
+        for(int i = 0 ; i < nums.size() ; i++) 
+        {
+            ans += to_string(nums[i]);
+        }
+
+        if(ans[0] == '0')
+           return "0";
+           
+        return ans;
+    }
+};
