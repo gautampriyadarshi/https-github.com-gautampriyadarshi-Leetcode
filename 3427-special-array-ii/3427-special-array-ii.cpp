bbed297c6d9eb9ffaefab9ea3eca2758de92ada2
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2024-12-09 10:11:44 +0530

    Time: 44 ms (28.13%), Space: 136.5 MB (22.19%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -2,34 +2,6 @@ class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) 
     {
-        // TLE: 523/536 cases passed:
-        /*  int n = queries.size();
-        vector<bool> ans;
-        int i = 0;
-        while(i < queries.size())
-        {
-            bool written = false;
-            int m = ans.size();
-            int x = queries[i][0], y = queries[i][1];
-            for(int j = x; j < y; j++)
-            {
-                if((nums[j] % 2 == 0 && nums[j+1] % 2 == 0) || (nums[j] % 2 != 0 && nums[j+1] % 2 != 0))
-                {
-                    if(written == false)
-                    {
-                        ans.push_back(0);
-                        written = true;
-                    }
-                }
-            }
-            
-            if(ans.size() == m)
-                ans.push_back(1);
-            
-            i++;
-        }
-        return ans; */
-
         int n = nums.size();
         vector<int> arr(n, n);
         vector<bool> ans;
@@ -49,7 +21,6 @@ public:
             else 
                 ans.push_back(true);
         }
-
         return ans;
     }
 };
