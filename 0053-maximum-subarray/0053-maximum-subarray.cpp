96be7fd4cefa314274f4614683a7e42a62f4a2c0
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-02-16 19:22:18 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.67%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -6,8 +6,10 @@ public:
         for(int i = 0; i < nums.size(); i++)
         {
             sum += nums[i];
-            maxi = max(maxi, sum);
 
+            if(sum > maxi)
+                maxi = max(maxi, sum);
+            
             if(sum < 0)
                 sum = 0;
         }
