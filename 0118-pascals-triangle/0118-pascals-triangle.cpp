b5b0aee1d5823ed0a83b5678dc6c0e55c669d483
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-02-16 19:56:22 +0530

    Time: 0 ms (100%), Space: 10.1 MB (5.55%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -2,48 +2,40 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) 
     {
-        if(numRows == 1)
-            return {{1}};
-
-        vector<vector<int>> dp(numRows, vector<int>(numRows, 0));
-        for(int i = 0; i < numRows; i++)
+        vector<vector<int>> vec(numRows, vector<int>(numRows, -1));
+        for(int i = 0; i < vec.size(); i++)
         {
-            for(int j = 0; j < numRows; j++)
+            for(int j = 0; j < vec[0].size(); j++)
             {
                 if(j == 0)
-                    dp[i][j] = 1;
-                
+                    vec[i][j] = 1;
+
                 if(i == j)
-                    dp[i][j] = 1;
+                    vec[i][j] = 1;
             }
         }
 
-        if(numRows == 2)
-            return {{1}, {1,1}};
-
-        for(int i = 2; i < numRows; i++)
+        for(int i = 2; i < vec.size(); i++)
         {
-            for(int j = 1; j < numRows; j++)
+            for(int j = 0; j < vec[0].size(); j++)
             {
-                if(i != j)
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+                if(j != 0 && i != j)
+                    vec[i][j] = vec[i-1][j] + vec[i-1][j-1];
             }
         }
 
         vector<vector<int>> ans;
-        for(int i = 0; i < dp.size(); i++)
+        for(int i = 0; i < vec.size(); i++)
         {
             vector<int> help;
-            for(int j = 0; j < dp[0].size(); j++)
+            for(int j = 0; j < vec[0].size(); j++)
             {
-                if(dp[i][j] != 0)
-                    help.push_back(dp[i][j]);
+                if(vec[i][j] > 0)
+                    help.push_back(vec[i][j]);
             }
             ans.push_back(help);
-            help.clear();
         }
-        dp.clear();
-        
+
         return ans;
     }
 };
