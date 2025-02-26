0606fccb3d85ecab09fcafc1e71139472a13b836
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-02-26 14:46:09 +0530

    Time: 4 ms (27.04%), Space: 14.1 MB (61.52%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,32 +1,82 @@
+/* Giving correct output of all cases but memory limit exceeded:
 class Solution {
-    void combinations (vector<int> &ds, int index, int target, vector<vector<int>> &ans, vector<int> &candidates) 
+    bool totalSum(vector<int> &subset, int target)
     {
-        if (target==0) 
+        int x = 0;
+        for(int i = 0; i < subset.size(); i++)
         {
-            ans.push_back(ds);
+            x += subset[i];
+        }
+        
+        if(x == target)
+            return true;
+        
+        return false;
+    }
+    void findSubset(int i, int target, set<vector<int>> &st, vector<int>& candidates, vector<int> subset)
+    {
+        if(i == candidates.size())
+        {
+            if(totalSum(subset, target))
+            {
+                sort(subset.begin(), subset.end());
+                st.insert(subset);
+            }
             return;
         }
-
-        for (int i = index; i < candidates.size(); i++) 
-        {
-            if (candidates[i] > target) 
-                break;
-
-            if (i >= 1 && i > index && candidates[i] == candidates[i-1]) 
-                continue;
-
-            ds.push_back(candidates[i]);
-            combinations(ds, i+1, target-candidates[i], ans, candidates);
-            ds.pop_back();
-        }
-    }
+        
+        subset.push_back(candidates[i]);
+        findSubset(i+1, target, st, candidates, subset);
+        subset.pop_back();
+        findSubset(i+1, target, st, candidates, subset);
+    }   
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) 
     {
+        vector<int> subset;
+        set<vector<int>> st;
+        findSubset(0, target, st, candidates, subset);
+
         vector<vector<int>> ans;
-        vector<int> ds = {};
-        sort(candidates.begin(), candidates.end());
-        combinations(ds, 0, target, ans, candidates);
+        for(auto x: st)
+        {
+            ans.push_back(x);
+        }
         return ans;
+    }
+};  */
+
+class Solution {
+    void backtrack(vector<int> &candidates, vector<int> &output,vector<vector<int>> &ans ,int index, int target)
+    {
+        if(target == 0)
+        {
+            ans.push_back(output) ;
+            return;
+        }
+
+        if(target < 0 || index >= candidates.size())
+            return;
+
+        for(int i = index ; i < candidates.size() ; i++)
+        {
+            if(i > index && candidates[i] == candidates[i-1])
+                continue;
+            
+            output.push_back(candidates[i]);
+            backtrack(candidates, output, ans, i+1, target-candidates[i]);
+            output.pop_back();
+        }
+    }
+
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) 
+    {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>>ans ;
+        vector<int>output ;
+        int index = 0 ;
+        backtrack(candidates, output, ans, index, target);
+        return ans ;
     }
 };
