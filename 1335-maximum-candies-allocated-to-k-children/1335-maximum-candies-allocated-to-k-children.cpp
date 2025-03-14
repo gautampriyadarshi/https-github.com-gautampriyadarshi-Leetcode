2c1774531753188d4937cd561433618afc6f894d
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-03-14 13:18:46 +0530

    Time: 41 ms (23.93%), Space: 88.1 MB (97.17%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    bool isit(vector<int>& candies, long long k, long long part) {
+        long long count = 0;
+        for (int &i : candies) {
+            count = count + (i / part);
+        }
+        return count >= k;
+    }
+public:
+    int maximumCandies(vector<int>& candies, long long k) 
+    {
+        sort(candies.begin(), candies.end());
+        long long sum = 0;
+        int mini = INT_MAX;
+        for (int &i : candies) {
+            sum += i;
+            mini = min(mini, i);
+        }
+
+        if (sum < k) return 0;
+
+        long long low = 1, high = sum / k, ans = 0;
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+            if (isit(candies, k, mid)) {
+                ans = mid;
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
