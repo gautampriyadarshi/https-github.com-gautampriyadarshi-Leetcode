daf34abb3554af5d8b675c6379a694196e1867e4
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-04-22 12:23:56 +0530

    Time: 48 ms (18.26%), Space: 53.6 MB (43.35%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -3,8 +3,7 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) 
-{
+var map = function(arr, fn) {
     for(let i = 0; i < arr.length; i++)
     {
         arr[i] = fn(arr[i], i);
