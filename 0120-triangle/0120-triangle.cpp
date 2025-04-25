e74142e26c341e7b0c8f65107635f53e1203bdac
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-04-25 23:14:45 +0530

    Time: 2 ms (18.49%), Space: 12.7 MB (70.14%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,9 +1,63 @@
 class Solution {
+    int Rec(int i, int j, vector<vector<int>> &triangle, int n)
+    {
+        if (i == n - 1)
+            return triangle[n - 1][j];
+
+        int down = triangle[i][j] + Rec(i + 1, j, triangle, n);
+        int diagonal = triangle[i][j] + Rec(i + 1, j + 1, triangle, n);
+
+        return min(down, diagonal);
+    }
+
+    int Mem(int i, int j, vector<vector<int>> &triangle, int n, vector<vector<int>> &dp)
+    {
+        if (i == n - 1)
+            return triangle[n - 1][j];
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int down = triangle[i][j] + Mem(i + 1, j, triangle, n, dp);
+        int diagonal = triangle[i][j] + Mem(i + 1, j + 1, triangle, n, dp);
+
+        return dp[i][j] = min(down, diagonal);
+    }
+
+    int Tab(vector<vector<int>> &triangle, int n)
+    {
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int j = 0; j < n; j++)
+        {
+            dp[n - 1][j] = triangle[n - 1][j];
+        }
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            for (int j = i; j >= 0; j--)
+            {
+                int down = triangle[i][j] + dp[i + 1][j];
+                int diagonal = triangle[i][j] + dp[i + 1][j + 1];
+                dp[i][j] = min(down, diagonal);
+            }
+        }
+        return dp[0][0];
+    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) 
     {
         int m = triangle.size();
         int n = triangle[m-1].size();
+
+        // Recursion:
+        // return Rec(0, 0, triangle, n);
+
+        // Memoization:
+        // vector<vector<int>> dp(n, vector<int>(n, -1));
+        // return Mem(0, 0, triangle, n, dp);
+
+        // Tabulation:
+        // return Tab(triangle, n);
 
         // Space Optimization:
         vector<int> front(n, 0), curr(n, 0);
