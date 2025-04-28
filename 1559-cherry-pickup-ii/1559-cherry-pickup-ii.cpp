2dd2e9df18362b84dd2bd55146e8c0e9841fc40d
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-04-29 00:28:28 +0530

    Time: 68 ms (27.95%), Space: 21 MB (27.6%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -65,6 +65,58 @@ class Solution {
         }
         return dp[i][j1][j2] = maxi;
     }
+
+    int Tab(int n, int m, vector<vector<int>> &grid)
+    {
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+
+        for(int j1 = 0; j1<m; j1++)
+        {
+            for(int j2 = 0; j2<m; j2++)
+            {
+                if(j1 == j2) dp[n-1][j1][j1] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+
+        for(int i = n-2; i>=0; i--)
+        {
+            for(int j1 = 0; j1<m; j1++)
+            {
+                for(int j2 = 0; j2<m; j2++)
+                {
+                    int maximum = INT_MIN;
+                    for(int k1 = -1; k1<=1; k1++)
+                    {
+                        int sum =0;
+                        for(int k2 = -1; k2<=1; k2++)
+                        {
+                            if(j1 == j2)
+                            {
+                                sum = grid[i][j1];
+                            }
+                            else
+                            {
+                                sum = grid[i][j1] + grid[i][j2];
+                            }
+                            if(j1+k1 < 0 || j1+k1 >=m || j2+k2<0 || j2+k2 >=m)
+                            {
+                                sum += -1e9;
+                            }
+                            else
+                            {
+                                sum += dp[i+1][j1+k1][j2+k2];
+                            }
+                            maximum = max(maximum, sum);
+                        }
+                    }
+                    dp[i][j1][j2] = maximum;  
+                }
+            }
+        }
+        return dp[0][0][m-1];
+    }
+
 public:
     int cherryPickup(vector<vector<int>>& grid) 
     {
@@ -74,7 +126,10 @@ public:
 
         // Memoization:
         // int dp[r][c][c]
-        vector<vector<vector<int>>> dp(r, vector<vector<int>>(c, vector<int>(c, -1)));
-        return Mem(0, 0, c - 1, r, c, grid, dp);
+        // vector<vector<vector<int>>> dp(r, vector<vector<int>>(c, vector<int>(c, -1)));
+        // return Mem(0, 0, c - 1, r, c, grid, dp);
+
+        // Tabulation:
+        return Tab(r, c, grid);
     }
 };
