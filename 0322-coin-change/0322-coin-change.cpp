39ff527897f27aae6f152e16e9bcf254c018482b
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-05-12 23:39:41 +0530

    Time: 33 ms (53.31%), Space: 21.9 MB (50.04%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -74,6 +74,42 @@ class Solution {
 
         return ans;
     }
+
+    int SpaceOptimization(vector<int>& num, int target)
+    {
+        int n = num.size();
+        vector<int> prev(target + 1, 0), curr(target + 1, 0);
+
+        for (int T = 0; T <= target; T++)
+        {
+            if (T % num[0] == 0)
+                prev[T] = T / num[0];
+            else
+                prev[T] = 1e9;
+        }
+
+        for (int ind = 1; ind < n; ind++)
+        {
+            for (int T = 0; T <= target; T++)
+            {
+                int notTake = 0 + prev[T];
+                int take = INT_MAX;
+
+                if (num[ind] <= T)
+                    take = 1 + curr[T - num[ind]];
+
+                curr[T] = min(take, notTake);
+            }
+            prev = curr;
+        }
+
+        int ans = prev[target];
+
+        if (ans >= 1e9)
+            return -1;
+
+        return ans;
+    }
 public:
     int coinChange(vector<int>& num, int target) 
     {
@@ -87,7 +123,10 @@ public:
         // int ans = Mem(n - 1, target, num, dp);
 
         // Tabulation:
-        int ans = Tab(num, target);
+        // int ans = Tab(num, target);
+
+        // Space Optimization:
+        int ans = SpaceOptimization(num, target);
 
         if (ans >= 1e9)
             return -1;
