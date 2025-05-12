b90381132c24f3ca560a2453e271a3a50831c1a3
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-05-13 00:09:51 +0530

    Time: 4 ms (80.78%), Space: 12.6 MB (64.99%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,7 +1,69 @@
 class Solution {
+    int Mem(int ind, int sum, vector<int> &num, vector<vector<int>> &dp)
+    {
+        // Base case modified from dp17, to handle if (num[ind] == 0) else everything is same.
+        if (ind == 0)
+        {
+            if (sum == 0 && num[0] == 0)
+                return 2;
+
+            if (sum == 0 || num[0] == sum)
+                return 1;
+
+            return 0;
+        }
+
+        if (dp[ind][sum] != -1)
+            return dp[ind][sum];
+
+        int notTake = Mem(ind - 1, sum, num, dp);
+        int take = 0;
+
+        if (num[ind] <= sum)
+            take = Mem(ind - 1, sum - num[ind], num, dp);
+
+        return dp[ind][sum] = (notTake + take);
+    }
+
+    int Tab(vector<int> &num, int tar)
+    {
+        int n = num.size();
+        vector<vector<int>> dp(n, vector<int>(tar + 1, 0));
+
+        if (num[0] == 0)
+            dp[0][0] = 2;
+        else
+            dp[0][0] = 1;
+
+        if (num[0] != 0 && num[0] <= tar)
+            dp[0][num[0]] = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int sum = 0; sum <= tar; sum++)
+            {
+                int notTake = dp[i - 1][sum];
+                int take = 0;
+
+                if (num[i] <= sum)
+                    take = dp[i - 1][sum - num[i]];
+
+                dp[i][sum] = (notTake + take);
+            }
+        }
+        return dp[n - 1][tar];
+    }
+
     int findWays(vector<int> &num, int tar)
     {
         int n = num.size();
+
+        // Memoization:
+        // vector<vector<int>> dp(n, vector<int>(tar + 1, -1));
+        // return Mem(n - 1, tar, num, dp);
+
+        // Tabulation:
+        // return Tab(num, tar);
 
         // Space Optimization:
         vector<int> prev(tar + 1, 0), curr(tar + 1, 0);
