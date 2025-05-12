ec00f556ce6e642b0152a8176a9b9200368ac8de
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-05-12 23:32:55 +0530

    Time: 51 ms (25.02%), Space: 49 MB (34.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -39,6 +39,41 @@ class Solution {
 
         return dp[ind][T] = min(take, notTake);
     }
+
+    int Tab(vector<int> &num, int target)
+    {
+        int n = num.size();
+        vector<vector<int>> dp(n, vector<int>(target + 1, 0));
+
+        for (int T = 0; T <= target; T++)
+        {
+            if (T % num[0] == 0)
+                dp[0][T] = T / num[0];
+            else
+                dp[0][T] = 1e9;
+        }
+
+        for (int ind = 1; ind < n; ind++)
+        {
+            for (int T = 0; T <= target; T++)
+            {
+                int notTake = 0 + dp[ind - 1][T];
+                int take = INT_MAX;
+
+                if (num[ind] <= T)
+                    take = 1 + dp[ind][T - num[ind]];
+
+                dp[ind][T] = min(take, notTake);
+            }
+        }
+
+        int ans = dp[n - 1][target];
+
+        if (ans >= 1e9)
+            return -1;
+
+        return ans;
+    }
 public:
     int coinChange(vector<int>& num, int target) 
     {
@@ -48,8 +83,11 @@ public:
         // int ans = Rec(n - 1, target, num);
 
         // Memoization:
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
-        int ans = Mem(n - 1, target, num, dp);
+        // vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        // int ans = Mem(n - 1, target, num, dp);
+
+        // Tabulation:
+        int ans = Tab(num, target);
 
         if (ans >= 1e9)
             return -1;
