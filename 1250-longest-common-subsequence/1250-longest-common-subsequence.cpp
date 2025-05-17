7f8e804517b01862430d6e2de3e1ee9db0cc98cd
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-05-17 20:42:19 +0530

    Time: 43 ms (22.98%), Space: 27.5 MB (27.65%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -0,0 +1,72 @@
+class Solution {
+    int Rec(int i, int j, string &s, string &t)
+    {
+        if (i < 0 || j < 0)
+            return 0;
+
+        if (s[i] == t[j])
+            return 1 + Rec(i - 1, j - 1, s, t);
+
+        return max(Rec(i - 1, j, s, t), Rec(i, j - 1, s, t));
+    }
+
+    int Mem(int i, int j, string &s, string &t, vector<vector<int>> &dp)
+    {
+        if (i < 0 || j < 0)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (s[i] == t[j])
+            return dp[i][j] = 1 + Mem(i - 1, j - 1, s, t, dp);
+
+        return dp[i][j] = max(Mem(i - 1, j, s, t, dp), Mem(i, j - 1, s, t, dp));
+    }
+
+    int Tab(string s, string t)
+    {
+        int n = s.size() + 1, m = t.size() + 1;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        // Base Case by shifting the index:
+        for (int i = 0; i <= n; i++)
+        {
+            dp[i][0] = 0;
+        }
+
+        for (int j = 0; j <= m; j++)
+        {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (s[i - 1] == t[j - 1]) // (__-1) due to shifting of index.
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[n][m];
+    }
+
+public:
+    int longestCommonSubsequence(string s, string t) 
+    {
+        int n = s.size(), m = t.size();
+
+        // Recursion:
+        // return Rec(n - 1, m - 1, s, t);
+
+        // Memoization:
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return Mem(n - 1, m - 1, s, t, dp);
+
+        // Tabulation:
+        // return Tab(s, t);
+    }
+};
