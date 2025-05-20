aa953a6997e3bcbb75680617e198373b6e040f50
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-05-20 19:43:41 +0530

    Time: 31 ms (56.05%), Space: 44.2 MB (39.25%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -13,7 +13,7 @@ class Solution {
         return Rec(i-1, j, s, t); 
     }
 
-    int Mem(int i, int j, string s, string t, vector<vector<int>> &dp) 
+    int Mem(int i, int j, string &s, string &t, vector<vector<int>> &dp) 
     { 
         if(j < 0) 
             return 1; 
@@ -29,6 +29,29 @@ class Solution {
         
         return dp[i][j] = Mem(i-1, j, s, t, dp); 
     }
+
+    int Tab(string s, string t)
+    {
+        int n = s.size(), m = t.size();
+        vector<vector<double>> dp(n+1, vector<double>(m+1, 0));
+
+        for(int i = 0; i <= n; i++)
+        {
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
+                if(s[i-1] == t[j-1]) 
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]; 
+                else
+                    dp[i][j] = dp[i-1][j]; 
+            }
+        }
+        return (int)dp[n][m];
+    }
 public:
     int numDistinct(string s, string t) 
     {
@@ -38,7 +61,10 @@ public:
         // return Rec(n-1, m-1, s, t);
 
         // Memoization:
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return Mem(n-1, m-1, s, t, dp);
+        // vector<vector<int>> dp(n, vector<int>(m, -1));
+        // return Mem(n-1, m-1, s, t, dp);
+
+        // Tabulation:
+        return Tab(s, t);
     }
 };
