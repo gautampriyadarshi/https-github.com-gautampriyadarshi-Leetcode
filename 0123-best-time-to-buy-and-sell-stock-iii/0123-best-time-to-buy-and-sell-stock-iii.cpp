69c0da32793b0711f90e35baf88e95777ac45d3a
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-05-24 12:46:46 +0530

    Time: 53 ms (63.82%), Space: 79.3 MB (79.28%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -63,6 +63,35 @@ class Solution {
         }
         return dp[0][1][2];
     }
+
+    int spaceOptimization(vector<int>& prices)
+    {
+        int n = prices.size();
+        vector<vector<int>> after(2, vector<int>(3, 0));
+        vector<vector<int>> curr(2, vector<int>(3, 0));
+
+        // Base cases done as all values are 0 initially.
+
+        for(int ind = n-1; ind >= 0; ind--)
+        {
+            for(int buy = 0; buy <= 1; buy++)
+            {
+                for(int cap = 1; cap <= 2; cap++)
+                {
+                    int profit = 0; 
+
+                    if(buy) 
+                        profit = max(-prices[ind] + after[0][cap], 0 + after[1][cap]); 
+                    else 
+                        profit = max(prices[ind] + after[1][cap-1], 0 + after[0][cap]); 
+
+                    curr[buy][cap] = profit;
+                }
+            }
+            after = curr;
+        }
+        return after[1][2];
+    }
 public:
     int maxProfit(vector<int>& prices) 
     {
@@ -76,7 +105,9 @@ public:
         // return Mem(0, 1, 2, prices, n, dp);
 
         // Tabulation:
-        return Tab(prices);
+        // return Tab(prices);
 
+        // Space Optimization:
+        return spaceOptimization(prices);
     }
 };
