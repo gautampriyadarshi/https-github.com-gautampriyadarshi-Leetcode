7af8e62641f13c10c9707e15a0e23be8b8a6040b
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-05-25 13:46:59 +0530

    Time: 564 ms (13.73%), Space: 296.5 MB (11.74%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    int Rec(int ind, int prev_ind, vector<int>& nums, int n) 
+    { 
+        if(ind == n) 
+            return 0; 
+
+        int len = 0 + Rec(ind+1, prev_ind, nums, n); 
+
+        if (prev_ind == -1 || nums[ind] > nums[prev_ind]) 
+            len = max(len, 1 + Rec(ind+1, ind, nums, n)); 
+        
+        return len; 
+    }
+
+    int Mem(int ind, int prev_ind, vector<int>& nums, int n, vector<vector<int>> &dp) 
+    { 
+        if(ind == n) 
+            return 0; 
+
+        if(dp[ind][prev_ind + 1] != -1)
+            return dp[ind][prev_ind + 1];
+
+        int len = 0 + Mem(ind+1, prev_ind, nums, n, dp); 
+
+        if (prev_ind == -1 || nums[ind] > nums[prev_ind]) 
+            len = max(len, 1 + Mem(ind+1, ind, nums, n, dp)); 
+        
+        return dp[ind][prev_ind + 1] = len; 
+    }
+public:
+    int lengthOfLIS(vector<int>& nums) 
+    {
+        int n = nums.size();
+
+        // Recursion:
+        // return Rec(0, -1, nums, n);
+
+        // Mempization:
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        return Mem(0, -1, nums, n, dp);
+    }
+};
