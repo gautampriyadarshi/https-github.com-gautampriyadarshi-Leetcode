1cb791b87130388ca88b7b81992b179f8e3ce47a
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-05-27 13:25:24 +0530

    Time: 4 ms (98.18%), Space: 12.6 MB (67.84%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,33 +1,46 @@
 class Solution {
+    vector<int> optimizedTab(vector<int>& nums)
+    {
+        // Same code as Longest Increasing Subsequence with a condition changed.
+        
+        int n = nums.size(), maxi = 1, lastIndex = 0;
+        vector<int> dp(n, 1), hash(n);
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n; i++)
+        {
+            hash[i] = i;
+            for (int prev = 0; prev < i; prev++)
+            {
+                if (nums[i] % nums[prev] == 0 && 1 + dp[prev] > dp[i])
+                {
+                    dp[i] = 1 + dp[prev];
+                    hash[i] = prev;
+                }
+            }
+
+            if (dp[i] > maxi)
+            {
+                maxi = dp[i];
+                lastIndex = i;
+            }
+        }
+
+        vector<int> temp;
+        temp.push_back(nums[lastIndex]);
+
+        while (hash[lastIndex] != lastIndex)
+        {
+            lastIndex = hash[lastIndex];
+            temp.push_back(nums[lastIndex]);
+        }
+        reverse(temp.begin(), temp.end());
+
+        return temp;
+    }
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) 
     {
-        vector<int> ans;
-        int n = nums.size(), maxi = 1, num = -1;
-        sort(nums.begin(), nums.end());
-        vector<int> dp(n, 1);
-
-        for(int i = 1; i < n; i++)
-        {
-            for(int j = 0; j < i; j++)
-            {
-                if(!(nums[i] % nums[j]) && (dp[i] < dp[j]+1))
-                    dp[i] = dp[j]+1;
-                
-                if(maxi < dp[i])
-                    maxi = dp[i];
-            }
-        }
-
-        for(int i = n-1; i >= 0; i--)
-        {
-            if(maxi == dp[i] && (num == -1 || !(num % nums[i])))
-            {
-                ans.push_back(nums[i]);
-                maxi--;
-                num = nums[i];
-            }
-        }
-        return ans;
+        return optimizedTab(nums);
     }
 };
