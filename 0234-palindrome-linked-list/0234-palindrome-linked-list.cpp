51d24fada6aae4cb06c7891a1b44391ebefdaf98
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-12-13 13:31:08 +0530

    Time: 8 ms (23.63%), Space: 127.6 MB (22.37%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -12,30 +12,26 @@ class Solution {
 public:
     bool isPalindrome(ListNode* head) 
     {
-        if (!head || !head->next) return true;
+        if(head->next == NULL)
+            return true;
+        
+        stack<int> st;
+        ListNode* node = head;
 
-        // Find the middle of the list
-        ListNode *slow = head, *fast = head;
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
+        while(node != NULL)
+        {
+            st.push(node->val);
+            node = node->next;
         }
 
-        // Reverse the second half of the list
-        ListNode *prev = nullptr, *curr = slow, *temp;
-        while (curr) {
-            temp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = temp;
-        }
-
-        // Compare the two halves
-        ListNode *first = head, *second = prev;
-        while (second) {
-            if (first->val != second->val) return false;
-            first = first->next;
-            second = second->next;
+        node = head;
+        while(node)
+        {
+            if(node->val != st.top())
+                return false;
+            
+            st.pop();
+            node = node->next;
         }
         return true;
     }
