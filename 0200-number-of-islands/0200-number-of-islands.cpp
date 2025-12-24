406eb2879feb4fe1b53173892512b89439ef6b90
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2025-12-24 20:38:04 +0530

    Time: 71 ms (8.04%), Space: 24.5 MB (15.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,57 +1,68 @@
 class Solution {
-    void bfs(int ro, int co, vector<vector<int>> &vis, vector<vector<char>> &grid)
+    void bfs(int ro, int co, vector<vector<int>> &vis, vector<vector<char>>& grid)
     {
         int n = grid.size();
         int m = grid[0].size();
         vis[ro][co] = 1;
+
         queue<pair<int, int>> q;
         q.push({ro, co});
 
-        while (!q.empty())
+        int dRow[] = {-1, 0, +1, 0};
+        int dCol[] = {0, +1, 0, -1};
+
+        while(!q.empty())
         {
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
 
-            // Traversing in the neighbours and pushing them into queue:
-            for (int delRow = -1; delRow <= 1; delRow++)
+            for(int i = 0; i < 4; i++)
             {
-                for (int delCol = -1; delCol <= 1; delCol++)
+                int nRow = dRow[i] + row;
+                int nCol = dCol[i] + col;
+
+                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && grid[nRow][nCol] == '1' && !vis[nRow][nCol])
                 {
-                    if((delRow == -1 && delCol == 1) || (delRow == 1 && delCol == 1) || (delRow == 1 && delCol == -1) || (delRow == -1 && delCol == -1))
-                        continue;
-
-                    int nRow = row + delRow; // nRow = neighbour Row
-                    int nCol = col + delCol; // nCol = neighbour Col
-
-                    if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && grid[nRow][nCol] == '1' && !vis[nRow][nCol])
-                    {
-                        vis[nRow][nCol] = 1;
-                        q.push({nRow, nCol});
-                    }
+                    vis[nRow][nCol] = 1;
+                    q.push({nRow, nCol});
                 }
             }
         }
     }
-
 public:
     int numIslands(vector<vector<char>>& grid) 
     {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
         int count = 0;
-        for (int row = 0; row < n; row++)
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        for(int i = 0; i < n; i++)
         {
-            for (int col = 0; col < m; col++)
+            for(int j = 0; j < m; j++)
             {
-                if (!vis[row][col] && grid[row][col] == '1')
+                if(!vis[i][j] && grid[i][j] == '1')
                 {
                     count++;
-                    bfs(row, col, vis, grid);
+                    bfs(i, j, vis, grid);
                 }
             }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                cout << vis[i][j] << " ";
+            }
+            cout << endl;
         }
         return count;
     }
 };
+
+// O(m*n) is for queue and other is for visited array
+
+// SC: O(m*n) + O(m*n)
+// TC: O(m*n)
