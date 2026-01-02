f23a01071abe61ffee4f716ba3bc639a7d67bedb
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-01-02 17:46:10 +0530

    Time: 7 ms (26.03%), Space: 31.9 MB (15.99%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,29 +1,60 @@
 class Solution {
-    int solve(vector<vector<int>>& grid, int row, int col, int i, int j)
+    int bfs(int ro, int co, vector<vector<int>> &vis, vector<vector<int>>& grid)
     {
-        if(i >= row || j >= col || i < 0 || j < 0 || grid[i][j] == 0 || grid[i][j] == -1)
-            return 0;
-        
-        int ans = grid[i][j];
-        grid[i][j] = -1;
+        int n = grid.size();
+        int m = grid[0].size();
+        int area = 0;
+        vis[ro][co] = 1;
 
-        ans += solve(grid, row, col, i+1, j) + solve(grid, row, col, i-1, j) +
-                solve(grid, row, col, i, j-1) + solve(grid, row, col, i, j+1);
-        
-        return ans;
+        queue<pair<int, int>> q;
+        q.push({ro, co});
+        area++;
+
+        int dRow[] = {-1, 0, +1, 0};
+        int dCol[] = {0, +1, 0, -1};
+
+        while(!q.empty())
+        {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for(int i = 0; i < 4; i++)
+            {
+                int nRow = dRow[i] + row;
+                int nCol = dCol[i] + col;
+
+                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m 
+                && grid[nRow][nCol] == 1 && !vis[nRow][nCol])
+                {
+                    
+                    vis[nRow][nCol] = 1;
+                    q.push({nRow, nCol});
+                    area++;
+                }
+            }
+        }
+        return area;
     }
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) 
     {
-        int ans = 0;
-        int row = grid.size(), col = grid[0].size();
-        for(int i = 0; i < row; i++)
+        int n = grid.size();
+        int m = grid[0].size();
+        int maxArea = 0;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        for(int i = 0; i < n; i++)
         {
-            for(int j = 0; j < col; j++)
+            for(int j = 0; j < m; j++)
             {
-                ans = max(ans, solve(grid, row, col, i, j));
+                if(!vis[i][j] && grid[i][j] == 1)
+                {
+                    int x = bfs(i, j, vis, grid);
+                    maxArea = max(maxArea, x);
+                }
             }
         }
-        return ans;
+        return maxArea;
     }
 };
