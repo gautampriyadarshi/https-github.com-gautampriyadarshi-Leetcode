eef5d051e598f10d6e1b5e71a74b7a41bde875d0
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-01-04 12:32:25 +0530

    Time: 4 ms (67.33%), Space: 23.8 MB (59.24%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,24 +5,29 @@ public:
         vector<vector<int>> ans;
         sort(intervals.begin(), intervals.end());
 
-        vector<int> help = intervals[0];
+        // vector<int> help = intervals[0];
+        int x1 = intervals[0][0], y1 = intervals[0][1];
         for(int i = 1; i < intervals.size(); ++i)
         {
-            vector<int> vec = intervals[i];
+            // vector<int> vec = intervals[i];  // {x, y}
+            int x2 = intervals[i][0], y2 = intervals[i][1];
 
-            if(help[1] >= vec[0]) {
-                help[1] = max(help[1], vec[1]);
+            if(y1 >= x2) {
+                y1 = max(y1, y2);
             }
             else {
-                ans.push_back(help);
-                help = vec;
-                vec.clear();
+                ans.push_back({x1, y1});
+                x1 = x2;
+                y1 = y2;
+                // help = {x2, y2};
+                // vec.clear();
             }
         }
 
-        ans.push_back(help);
-        help.clear();
+        ans.push_back({x1, y1});
+        // help.clear();
 
         return ans;
     }
 };
+// In new change optimized the space and now not using two extra vectors of N space.
