ac738e9166ff40f59ba21d2735a295a03b5ca243
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-04-22 22:06:39 +0530

    Time: 0 ms (100%), Space: 19.7 MB (74.15%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,4 +1,3 @@
-#include<bits/stdc++.h>
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -14,36 +13,48 @@ class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) 
     {
-        if(!root)
+        if (!root)
             return 0;
-        
-        int width = 0;
-        queue<pair<TreeNode*, long long>> q;
-        q.push({root, 0});
 
-        while(!q.empty())
+        int maxWidth = 0;
+
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});  // Push root node with index 0
+
+        while (!q.empty()) 
         {
-            int size = q.size(), mini = q.front().second, first, last;
-            for(int i = 0; i < size; i++)
+            int size = q.size();
+            int minIndex = q.front().second;
+            int first = 0, last = 0;    // To track first and last index
+
+            for (int i = 0; i < size; i++) 
             {
-                int curr = q.front().second - mini;
+                int currIndex = q.front().second - minIndex;
+
                 TreeNode* node = q.front().first;
                 q.pop();
 
-                if(!i) 
-                    first = curr;
+                // Update the first index if it's the first node
+                if (i == 0)
+                    first = currIndex;
 
-                if(i == size-1) 
-                    last = curr;
+                // Update the last index if it's the last node
+                if (i == size - 1)
+                    last = currIndex;
 
-                if(node->left) 
-                    q.push({node->left, (long long)curr*2 + 1});
+                if (node->left)
+                    q.push({node->left, (long long)2 * currIndex + 1});
 
-                if(node->right) 
-                    q.push({node->right, (long long)curr*2 + 2});
+                if (node->right)
+                    q.push({node->right, (long long)2 * currIndex + 2});
             }
-            width = max(width, last - first + 1);
+            maxWidth = max(maxWidth, last - first + 1);
         }
-        return width;
+
+        return maxWidth;
     }
 };
+
+// For every left node : width = (2 * parentIndex + 1)
+// For every right node: width = (2 * parentIndex + 2)
+// Width we are finding by calculating with (rightMostIndex - leftMostIndex + 1).
