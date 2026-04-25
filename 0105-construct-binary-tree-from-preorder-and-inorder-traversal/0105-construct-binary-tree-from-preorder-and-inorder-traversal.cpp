6538cd20d28a69c6126b1b011d8e6e2741f8bc7b
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-04-25 17:33:24 +0530

    Time: 2 ms (79.44%), Space: 27.5 MB (32.87%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,8 +10,9 @@
  * };
  */
 class Solution {
-     TreeNode* construct(vector<int>& preorder, int preStart, int preEnd,        vector<int>& inorder,int inStart, int inEnd, map<int,int> &mp)
-     {
+     TreeNode* construct(vector<int>& preorder, int preStart, int preEnd, 
+        vector<int>& inorder, int inStart, int inEnd, map<int,int> &mp)
+    {
         if(preStart > preEnd || inStart > inEnd) 
             return NULL;
 
@@ -19,9 +20,11 @@ class Solution {
         int inRoot = mp[root->val];
         int numsLeft = inRoot-inStart;
         
-        root->left = construct(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot-1, mp);
+        root->left = construct(preorder, preStart + 1, preStart + numsLeft, 
+                                inorder, inStart, inRoot-1, mp);
 
-        root->right = construct(preorder, preStart + numsLeft + 1, preEnd,inorder, inRoot + 1, inEnd, mp);
+        root->right = construct(preorder, preStart + numsLeft + 1, preEnd, 
+                                inorder, inRoot + 1, inEnd, mp);
         
         return root;
     }
@@ -29,13 +32,12 @@ public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) 
     {
         map<int,int> mp;
-        for(int i = 0; i < preorder.size(); i++)
-        {
+        for(int i = 0; i < preorder.size(); i++) {
             mp[inorder[i]] = i;
         }
 
-        TreeNode* root = construct(preorder, 0, preorder.size()-1, inorder, 0,inorder.size()-1, mp);
-        
+        TreeNode* root = construct(preorder, 0, preorder.size()-1, 
+                                    inorder, 0,inorder.size()-1, mp);
         return root;
     }
 };
