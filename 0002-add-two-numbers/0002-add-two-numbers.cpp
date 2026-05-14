782239de2682919d33e12f812a9f619fe6840376
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-05-14 18:53:12 +0530

    Time: 0 ms (100%), Space: 78.8 MB (11.58%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,168 +11,85 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
-    {
-        int m = 0, n = 0;
+    {   
+        if(!l1 && !l2)
+            return NULL;
+        
+        if(l1 && !l2)
+            return l1;
 
-        ListNode* n1 = l1;
-        while(n1 != NULL)
+        if(!l1 && l2)
+            return l2;
+
+        vector<int> vec, num1, num2;
+        ListNode* curr = l1;
+
+        while(curr != NULL)
         {
-            m++;
-            if(n1->next == NULL)
-                break;
-            
-            n1 = n1->next;
+            num1.push_back(curr->val);
+            curr = curr->next;
         }
 
-        ListNode* n2 = l2;
-        while(n2 != NULL)
+        curr = l2;
+        while(curr != NULL)
         {
-            n++;
-            if(n2->next == NULL)
-                break;
-
-            n2 = n2->next;
+            num2.push_back(curr->val);
+            curr = curr->next;
         }
 
-        if(m > n)
+        int m = num1.size(), n = num2.size();
+
+        if(m != n)
         {
             while(m > n)
             {
-                ListNode* node = new ListNode(0);
-                n2->next = node;
-                n2 = n2->next;
+                num2.push_back(0);
                 n++;
             }
-        }
-        else if(n > m)
-        {
-            while(n > m)
+
+            while(m < n)
             {
-                ListNode* node = new ListNode(0);
-                n1->next = node;
-                n1 = n1->next;
+                num1.push_back(0);
                 m++;
             }
         }
 
-        int carry = 0;
-        n1 = l1;
-        n2 = l2;
+        int j = 0, carry = 0;
+        for(int i = 0; i < num1.size(); i++)
+        {
+            int sum = num1[i] + num2[j] + carry;
 
-        ListNode* node = new ListNode;
-        ListNode* an = node;
-        int sum = n1->val + n2->val;
-        if(sum > 9)
-        {
-            node->val = (sum % 10);
-            carry = 1;
-        }
-        else
-        {
-            node->val = sum;
-            carry = 0;
-        }
-        n1 = n1->next;
-        n2 = n2->next;
-        
-        for(int i = 1; i < m; i++)
-        {
-            int sum = n1->val + n2->val + carry;
-
-            ListNode* nd = new ListNode;
-            if(sum > 9)
+            if(sum <= 9)
             {
-                nd->val = (sum % 10);
-                carry = 1;
-            }
-            else if(sum <= 9)
-            {
-                nd->val = sum;
+                vec.push_back(sum);
                 carry = 0;
             }
+            else
+            {
+                vec.push_back(sum % 10);
+                carry = 1;
+            }
 
-            node->next = nd;
-            node = nd;
-
-            n1 = n1->next;
-            n2 = n2->next;
+            j++;
         }
 
         if(carry != 0)
-        {
-            ListNode* nd = new ListNode(1);
-            node->next = nd;
-        }
+            vec.push_back(1);
 
-        return an;
+        ListNode* node = new ListNode(vec[0]);
+        curr = node;
+
+        for(int i = 1; i < vec.size(); i++)
+        {
+            ListNode* nd = new ListNode(vec[i]);
+            curr->next = nd;
+            curr = nd;
+        }
+        
+        vec.shrink_to_fit();
+        num1.shrink_to_fit();
+        num2.shrink_to_fit();
+
+        return node;
     }
-    /*
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
-    {
-        vector<int> vec1, vec2, ans;
-
-        ListNode* n1 = l1;
-        while(n1 != NULL)
-        {
-            vec1.push_back(n1->val);
-            n1 = n1->next;
-        }
-
-        ListNode* n2 = l2;
-        while(n2 != NULL)
-        {
-            vec2.push_back(n2->val);
-            n2 = n2->next;
-        }
-
-        int m = vec1.size(), n = vec2.size();
-        if(m > n)
-        {
-            while(m > n)
-            {
-                vec2.push_back(0);
-                n++;
-            }
-        }
-        else if(n > m)
-        {
-            while(n > m)
-            {
-                vec1.push_back(0);
-                m++;
-            }
-        }
-
-        int carry = 0;
-        for(int i = 0; i < vec1.size(); i++)
-        {
-            int sum = vec1[i] + vec2[i] + carry;
-
-            if(sum > 9)
-            {
-                ans.push_back(sum % 10);
-                carry = 1;
-            }
-            else if(sum <= 9)
-            {
-                ans.push_back(sum);
-                carry = 0;
-            }
-        }
-
-        if(carry != 0)
-            ans.push_back(1);
-        
-        ListNode* node = new ListNode(ans[0]);
-        ListNode* an = node;
-
-        for(int i = 1; i < ans.size(); i++)
-        {
-            ListNode* nd = new ListNode(ans[i]);
-            node->next = nd;
-            node = nd;
-        }
-        ans.clear(), vec1.clear(), vec2.clear();
-        return an;
-    }   */
 };
