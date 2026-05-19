df7d9985f39464ee98cac5c8b7eb5c2c18f70b56
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-05-19 21:19:32 +0530

    Time: 0 ms (100%), Space: 12.3 MB (40.12%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,18 +2,30 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
     {
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < nums1.size(); j++)
-            {
-                if(nums1[j] == 0)
-                {
-                    nums1[j] = nums2[i];
-                    break;
-                }
-            }
+        /* // TC: O((m+n)log(m+n))
+        for(int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
         }
+        sort(nums1.begin(), nums1.end());   */ 
 
-        sort(nums1.begin(), nums1.end());
+        /***********TC: O(m+n) ***********/
+        int i = m - 1;       
+        int j = n - 1;       
+        int k = m + n - 1;   // Pointer for the end of nums1 (where we place the largest element)
+        
+        while (j >= 0) 
+        {
+            // If nums1 still has valid elements and its current element is larger
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } 
+            else {
+                // Otherwise, the element from nums2 is larger (or nums1 is exhausted)
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
     }
 };
