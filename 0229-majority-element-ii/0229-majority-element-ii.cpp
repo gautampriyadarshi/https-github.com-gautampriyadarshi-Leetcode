1e7649d5d1d5cb5de5f05f517341dd652e382471
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-05-21 18:23:14 +0530

    Time: 3 ms (51.98%), Space: 23.7 MB (62.58%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,22 +2,55 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) 
     {
-        if(nums.size() == 1)
-            return nums;
+        int num1 = 0, num2 = 0;
+        int count1 = 0, count2 = 0;
         
-        int n = nums.size();
-        unordered_map<int,int> mp;
-        for(int i = 0; i < n; i++)
+        // Pass 1: Find potential candidates
+        for (int x : nums) 
         {
-            mp[nums[i]]++;
+            if (x == num1) 
+                count1++;
+            else if (x == num2) 
+                count2++;
+            else if (count1 == 0) 
+            {
+                num1 = x;
+                count1 = 1;
+            }
+            else if (count2 == 0) 
+            {
+                num2 = x;
+                count2 = 1;
+            }
+                
+            else {
+                // If the current number doesn't match either candidate,
+                // and both candidates have votes, we decrement both counts.
+                count1--;
+                count2--;
+            }
+        }
+        
+        // Pass 2: Verify if candidates actually appear more than n/3 times
+        count1 = 0;
+        count2 = 0;
+
+        for (int x : nums) 
+        {
+            if (x == num1) 
+                count1++;
+            else if (x == num2) 
+                count2++;
         }
         
         vector<int> ans;
-        for(auto x: mp)
-        {
-            if(x.second > n/3)
-                ans.push_back(x.first);
-        }
+        int n = nums.size();
+        
+        if (count1 > n / 3) 
+            ans.push_back(num1);
+        if (count2 > n / 3) 
+            ans.push_back(num2);
+        
         return ans;
     }
 };
