7230d1b003e631782c5f3cb2549172b50a5200b8
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-06-03 22:36:05 +0530

    Time: 122 ms (8.21%), Space: 148.6 MB (62.61%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,48 +1,43 @@
+// The Strategy:
+
+//1. We must ensure the heaps are balanced. The Max-Heap can either be the exact same size as the Min-Heap, or it can have exactly one more element than the Min-Heap.
+
+//2. When a new number arrives, we first push it to the Max-Heap.
+
+//3. To guarantee that all elements in the Max-Heap are smaller than those in the Min-Heap, we take the largest element from the Max-Heap (its top) and push it into the Min-Heap.
+
+//4. Finally, if the Min-Heap becomes larger than the Max-Heap, we move the top of the Min-Heap back to the Max-Heap to restore our size constraint.
+
 class MedianFinder {
-    priority_queue<int, vector<int>, greater<int> > minHeap;
-	priority_queue<int> maxHeap;
+private:
+    priority_queue<int> maxHeap; // Stores the smaller half of numbers
+    priority_queue<int, vector<int>, greater<int>> minHeap; // Stores the larger half
+
 public:
     MedianFinder() {}
     
     void addNum(int num) 
     {
+        // 1. Add to maxHeap
         maxHeap.push(num);
-
-        //check if all elements in maxHeap are smaller than all in minHeap.
-        //if not, then pop the top from maxHeap and push it to minHeap.
-        if(!minHeap.empty() && !maxHeap.empty() && maxHeap.top() > minHeap.top()) 
-        {
-            minHeap.push(maxHeap.top());
-            maxHeap.pop();
-        }
-
-        //check for balancing size of the heaps
-        if(maxHeap.size() > minHeap.size()+1) {
-            minHeap.push(maxHeap.top());
-            maxHeap.pop();
-        }
         
-        if(minHeap.size() > maxHeap.size()+1) {
+        // 2. Balance the values (ensure maxHeap values <= minHeap values)
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+        
+        // 3. Balance the sizes (maxHeap should always be >= minHeap in size)
+        if (maxHeap.size() < minHeap.size()) {
             maxHeap.push(minHeap.top());
             minHeap.pop();
         }
     }
     
-    double findMedian() 
-    {
-        if(minHeap.size() > maxHeap.size()) 
-            return minHeap.top();
-
-        if(maxHeap.size() > minHeap.size()) 
+    double findMedian() {
+        // If sizes are unequal, the extra element is in maxHeap
+        if (maxHeap.size() > minHeap.size()) 
             return maxHeap.top();
-        else 
-            return (minHeap.top() + maxHeap.top())/2.0;
+        
+        // If sizes are equal, average the two middle elements
+        return (maxHeap.top() + minHeap.top()) / 2.0;
     }
 };
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
