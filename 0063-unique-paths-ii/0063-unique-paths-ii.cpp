ac08fb50c001dfdeccf44e5130d0c732f081032c
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-07-03 23:27:23 +0530

    Time: 3 ms (9.98%), Space: 12.1 MB (6.19%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,71 +1,42 @@
 #define MOD 2000000000;
 class Solution {
-    int Rec(int i, int j, vector<vector<int>> &matrix)
+    int Rec(int i, int j, vector<vector<int>>& matrix)
     {
-        if (i >= 0 && j >= 0 && matrix[i][j] == 1) // Blockage represented by 1
+        // In any case when matrix[i][j] == 1 then path is invalid
+        if(i >= 0 && j >= 0 && matrix[i][j] == 1)
             return 0;
 
-        if (i == 0 && j == 0)
+        if(i == 0 && j == 0)
             return 1;
-
-        if (i < 0 || j < 0)
+        
+        if(i < 0 || j < 0)
             return 0;
+        
+        int left = Rec(i, j-1, matrix);
+        int up = Rec(i-1, j, matrix);
 
-        int up = Rec(i - 1, j, matrix);
-        int left = Rec(i, j - 1, matrix);
-
-        return (up + left) % MOD;
+        return (left + up) % MOD;
     }
 
-    int Mem(int i, int j, vector<vector<int>> &matrix, vector<vector<int>> &dp)
+    int Mem(int i, int j, vector<vector<int>>& matrix, vector<vector<int>> &dp)
     {
-        if (i >= 0 && j >= 0 && matrix[i][j] == 1) // Blockage represented by 1
+        if(i >= 0 && j >= 0 && matrix[i][j] == 1)
             return 0;
 
-        if (i == 0 && j == 0)
+        if(i == 0 && j == 0)
             return 1;
-
-        if (i < 0 || j < 0)
+        
+        if(i < 0 || j < 0)
             return 0;
 
-        if (dp[i][j] != -1)
+        if(dp[i][j] != -1)
             return dp[i][j];
+        
+        int left = Mem(i, j-1, matrix, dp);
+        int up = Mem(i-1, j, matrix, dp);
 
-        int up = Mem(i - 1, j, matrix, dp);
-        int left = Mem(i, j - 1, matrix, dp);
-
-        return dp[i][j] = (up + left) % MOD;
+        return dp[i][j] = (left + up) % MOD;
     }
-
-    int Tab(int m, int n, vector<vector<int>> &matrix)
-    {
-        int dp[m][n];
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (matrix[i][j] == 1) // Blockage represented by 1
-                    dp[i][j] = 0;
-
-                else if (i == 0 && j == 0)
-                    dp[i][j] = 1;
-
-                else
-                {
-                    int up = 0, left = 0;
-
-                    if (i > 0)
-                        up = dp[i - 1][j];
-                    if (j > 0)
-                        left = dp[i][j - 1];
-
-                    dp[i][j] = (up + left) % MOD;
-                }
-            }
-        }
-        return dp[m - 1][n - 1];
-    }
-
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& matrix) 
     {
