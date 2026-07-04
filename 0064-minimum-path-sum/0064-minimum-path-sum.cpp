716dab723deba429adf38199840d43a7a1939e9e
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-07-04 20:14:19 +0530

    Time: 0 ms (100%), Space: 16.3 MB (61.18%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,65 +1,35 @@
 class Solution {
-    int Rec(int i, int j, vector<vector<int>> &grid)
+    long long Rec(int i, int j, vector<vector<int>>& grid)
     {
-        if (i == 0 && j == 0)
+        if(i == 0 && j == 0)
             return grid[i][j];
 
-        if (i < 0 || j < 0)
+        if(i < 0 || j < 0)
             return INT_MAX;
+        
+        long long left = grid[i][j] + Rec(i, j-1, grid); 
+        long long up   = grid[i][j] + Rec(i-1, j, grid);
 
-        int up = grid[i][j] + Rec(i - 1, j, grid);
-        int left = grid[i][j] + Rec(i, j - 1, grid);
-
-        return min(up, left);
+        return min(left, up);
     }
 
-    int Mem(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp)
+    long long Mem(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &dp)
     {
-        if (i == 0 && j == 0)
+        if(i == 0 && j == 0)
             return grid[i][j];
 
-        if (i < 0 || j < 0)
+        if(i < 0 || j < 0)
             return INT_MAX;
 
-        if (dp[i][j] != -1)
+        if(dp[i][j] != -1)
             return dp[i][j];
+        
+        long long left = grid[i][j] + Mem(i, j-1, grid, dp); 
+        long long up   = grid[i][j] + Mem(i-1, j, grid, dp);
 
-        int up = grid[i][j] + Rec(i - 1, j, grid);
-        int left = grid[i][j] + Rec(i, j - 1, grid);
-
-        return dp[i][j] = min(up, left);
+        return dp[i][j] = min(left, up);
     }
-
-    int Tab(int m, int n, vector<vector<int>> &grid)
-    {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (i == 0 && j == 0)
-                    dp[i][j] = grid[i][j];
-                else
-                {
-                    int up = grid[i][j];
-                    if (i > 0)
-                        up += dp[i - 1][j];
-                    else
-                        up = INT_MAX;
-
-                    int left = grid[i][j];
-                    if (j > 0)
-                        left += dp[i][j - 1];
-                    else
-                        left = INT_MAX;
-
-                    dp[i][j] = min(up, left);
-                }
-            }
-        }
-        return dp[m - 1][n - 1];
-    }
-
+    
 public:
     int minPathSum(vector<vector<int>>& grid) 
     {
@@ -69,40 +39,8 @@ public:
         // return Rec(m-1, n-1, grid);
 
         // Memoization:
-        // vector<vector<int>> dp(m, vector<int>(n, -1));
-        // return Mem(m-1, n-1, grid, dp);
-
-        // Tabulation:
-        // return Tab(m-1, n-1, grid);
-
-        // Space Optimization:
-        vector<int> prev(n, 0);
-        for (int i = 0; i < m; i++)
-        {
-            vector<int> curr(n);
-            for (int j = 0; j < n; j++)
-            {
-                if (i == 0 && j == 0)
-                    curr[j] = grid[i][j];
-                else
-                {
-                    int up = grid[i][j];
-                    if (i > 0)
-                        up += prev[j];
-                    else
-                        up = INT_MAX;
-
-                    int left = grid[i][j];
-                    if (j > 0)
-                        left += curr[j - 1];
-                    else
-                        left = INT_MAX;
-
-                    curr[j] = min(up, left);
-                }
-            }
-            prev = curr;
-        }
-        return prev[n - 1];
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return Mem(m-1, n-1, grid, dp);
+        
     }
 };
