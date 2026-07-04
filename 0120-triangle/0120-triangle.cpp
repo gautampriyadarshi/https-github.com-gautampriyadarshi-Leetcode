ffa5b310f6f4744e4e946f2e7b34520a38c68989
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-07-05 00:24:35 +0530

    Time: 0 ms (100%), Space: 13.3 MB (58.35%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,81 +1,62 @@
 class Solution {
-    int Rec(int i, int j, vector<vector<int>> &triangle, int n)
+    int Rec(int i, int j, vector<vector<int>>& triangle, int m)
     {
-        if (i == n - 1)
-            return triangle[n - 1][j];
-
-        int down = triangle[i][j] + Rec(i + 1, j, triangle, n);
-        int diagonal = triangle[i][j] + Rec(i + 1, j + 1, triangle, n);
+        if(i == m-1)
+            return triangle[m-1][j];
+        
+        int down     = triangle[i][j] + Rec(i+1, j  , triangle, m);
+        int diagonal = triangle[i][j] + Rec(i+1, j+1, triangle, m);
 
         return min(down, diagonal);
     }
 
-    int Mem(int i, int j, vector<vector<int>> &triangle, int n, vector<vector<int>> &dp)
+    int Mem(int i, int j, vector<vector<int>>& triangle, int m, vector<vector<int>> &dp)
     {
-        if (i == n - 1)
-            return triangle[n - 1][j];
+        if(i == m-1)
+            return triangle[m-1][j];
 
-        if (dp[i][j] != -1)
+        if(dp[i][j] != -1)
             return dp[i][j];
-
-        int down = triangle[i][j] + Mem(i + 1, j, triangle, n, dp);
-        int diagonal = triangle[i][j] + Mem(i + 1, j + 1, triangle, n, dp);
+        
+        int down     = triangle[i][j] + Mem(i+1, j  , triangle, m, dp);
+        int diagonal = triangle[i][j] + Mem(i+1, j+1, triangle, m, dp);
 
         return dp[i][j] = min(down, diagonal);
     }
 
-    int Tab(vector<vector<int>> &triangle, int n)
+    int Tab(vector<vector<int>>& triangle, int m)
     {
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        for (int j = 0; j < n; j++)
-        {
-            dp[n - 1][j] = triangle[n - 1][j];
+        vector<vector<int>> dp(m, vector<int>(m, 0));
+        for (int j = 0; j < m; j++) {
+            dp[m - 1][j] = triangle[m - 1][j];
         }
 
-        for (int i = n - 2; i >= 0; i--)
+        for(int i = m-2; i >= 0; i--)
         {
-            for (int j = i; j >= 0; j--)
+            for(int j = i; j >= 0; j--)
             {
-                int down = triangle[i][j] + dp[i + 1][j];
-                int diagonal = triangle[i][j] + dp[i + 1][j + 1];
+                int down     = triangle[i][j] + dp[i+1][j];
+                int diagonal = triangle[i][j] + dp[i+1][j+1];
+
                 dp[i][j] = min(down, diagonal);
             }
         }
+
         return dp[0][0];
     }
 public:
     int minimumTotal(vector<vector<int>>& triangle) 
     {
         int m = triangle.size();
-        int n = triangle[m-1].size();
 
         // Recursion:
-        // return Rec(0, 0, triangle, n);
+        // return Rec(0, 0, triangle, m);
 
         // Memoization:
-        // vector<vector<int>> dp(n, vector<int>(n, -1));
-        // return Mem(0, 0, triangle, n, dp);
+        // vector<vector<int>> dp(m, vector<int>(m, -1));
+        // return Mem(0, 0, triangle, m, dp);
 
         // Tabulation:
-        // return Tab(triangle, n);
-
-        // Space Optimization:
-        vector<int> front(n, 0), curr(n, 0);
-        for (int j = 0; j < n; j++)
-        {
-            front[j] = triangle[n - 1][j];
-        }
-
-        for (int i = n - 2; i >= 0; i--)
-        {
-            for (int j = i; j >= 0; j--)
-            {
-                int down = triangle[i][j] + front[j];
-                int diagonal = triangle[i][j] + front[j + 1];
-                curr[j] = min(down, diagonal);
-            }
-            front = curr;
-        }
-        return front[0];
+        return Tab(triangle, m);
     }
 };
