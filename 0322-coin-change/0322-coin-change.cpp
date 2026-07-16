4eff6a5d78f3b0951d74b4fb8aa29c3c1c7b2e61
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-07-16 21:43:59 +0530

    Time: 47 ms (34.74%), Space: 49.4 MB (25.04%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,12 +1,12 @@
 class Solution {
     int Rec(int ind, int T, vector<int> &nums)
     {
-        if (ind == 0)
+        if(ind == 0)
         {
-            if (T % nums[0] == 0)
-                return T / nums[0];
-
-            return 1e9;
+            if(T % nums[ind] == 0)
+                return (T / nums[ind]);
+            
+            return INT_MAX-1;   // To prevent integer overflow
         }
 
         int notTake = 0 + Rec(ind - 1, T, nums);
@@ -20,15 +20,15 @@ class Solution {
 
     int Mem(int ind, int T, vector<int> &nums, vector<vector<int>> &dp)
     {
-        if (ind == 0)
+        if(ind == 0)
         {
-            if (T % nums[0] == 0)
-                return T / nums[0];
-
-            return 1e9;
+            if(T % nums[ind] == 0)
+                return (T / nums[ind]);
+            
+            return INT_MAX-1;   // To prevent integer overflow
         }
 
-        if (dp[ind][T] != -1)
+        if(dp[ind][T] != -1)
             return dp[ind][T];
 
         int notTake = 0 + Mem(ind - 1, T, nums, dp);
@@ -39,77 +39,6 @@ class Solution {
 
         return dp[ind][T] = min(take, notTake);
     }
-
-    int Tab(vector<int> &num, int target)
-    {
-        int n = num.size();
-        vector<vector<int>> dp(n, vector<int>(target + 1, 0));
-
-        for (int T = 0; T <= target; T++)
-        {
-            if (T % num[0] == 0)
-                dp[0][T] = T / num[0];
-            else
-                dp[0][T] = 1e9;
-        }
-
-        for (int ind = 1; ind < n; ind++)
-        {
-            for (int T = 0; T <= target; T++)
-            {
-                int notTake = 0 + dp[ind - 1][T];
-                int take = INT_MAX;
-
-                if (num[ind] <= T)
-                    take = 1 + dp[ind][T - num[ind]];
-
-                dp[ind][T] = min(take, notTake);
-            }
-        }
-
-        int ans = dp[n - 1][target];
-
-        if (ans >= 1e9)
-            return -1;
-
-        return ans;
-    }
-
-    int SpaceOptimization(vector<int>& num, int target)
-    {
-        int n = num.size();
-        vector<int> prev(target + 1, 0), curr(target + 1, 0);
-
-        for (int T = 0; T <= target; T++)
-        {
-            if (T % num[0] == 0)
-                prev[T] = T / num[0];
-            else
-                prev[T] = 1e9;
-        }
-
-        for (int ind = 1; ind < n; ind++)
-        {
-            for (int T = 0; T <= target; T++)
-            {
-                int notTake = 0 + prev[T];
-                int take = INT_MAX;
-
-                if (num[ind] <= T)
-                    take = 1 + curr[T - num[ind]];
-
-                curr[T] = min(take, notTake);
-            }
-            prev = curr;
-        }
-
-        int ans = prev[target];
-
-        if (ans >= 1e9)
-            return -1;
-
-        return ans;
-    }
 public:
     int coinChange(vector<int>& num, int target) 
     {
@@ -119,14 +48,8 @@ public:
         // int ans = Rec(n - 1, target, num);
 
         // Memoization:
-        // vector<vector<int>> dp(n, vector<int>(target + 1, -1));
-        // int ans = Mem(n - 1, target, num, dp);
-
-        // Tabulation:
-        // int ans = Tab(num, target);
-
-        // Space Optimization:
-        int ans = SpaceOptimization(num, target);
+        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        int ans = Mem(n - 1, target, num, dp);
 
         if (ans >= 1e9)
             return -1;
