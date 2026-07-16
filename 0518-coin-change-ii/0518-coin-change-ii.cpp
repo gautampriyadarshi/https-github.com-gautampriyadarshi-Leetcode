a4e1b0aa23640bd9015c607f66bc51e75dfd8c9f
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-07-16 21:57:53 +0530

    Time: 23 ms (73.7%), Space: 36.6 MB (48.24%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -6,6 +6,7 @@ class Solution {
 
         int notTake = Rec(ind - 1, T, a);
         int take = 0;
+
         if (a[ind] <= T)
             take = Rec(ind, T - a[ind], a);
 
@@ -27,53 +28,6 @@ class Solution {
 
         return dp[ind][T] = take + notTake;
     }
-
-    int Tab(vector<int> &a, int n, int amount)
-    {
-        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
-        for (int T = 0; T <= amount; T++)
-        {
-            dp[0][T] = (T % a[0] == 0);
-        }
-
-        for (int ind = 1; ind < n; ind++)
-        {
-            for (int T = 0; T <= amount; T++)
-            {
-                int notTake = dp[ind - 1][T];
-                int take = 0;
-                if (a[ind] <= T)
-                    take = dp[ind][T - a[ind]]; 
-
-                dp[ind][T] = take + notTake;
-            }
-        }
-        return dp[n - 1][amount];
-    }
-
-    int spaceOptimization(vector<int> &a, int n, int amount)
-    {
-        vector<int> prev(amount + 1, 0), curr(amount + 1, 0);
-        for (int T = 0; T <= amount; T++)
-        {
-            prev[T] = (T % a[0] == 0);
-        }
-
-        for (int ind = 1; ind < n; ind++)
-        {
-            for (int T = 0; T <= amount; T++)
-            {
-                int notTake = prev[T];
-                int take = 0;
-                if (a[ind] <= T)
-                    take = curr[T - a[ind]];
-
-                curr[T] = take + notTake; // Giving Run-time error:
-            }
-            prev = curr;
-        }
-        return prev[amount];
-    }
 public:
     int change(int amount, vector<int>& coins) 
     {
@@ -85,11 +39,5 @@ public:
         // Memoization:
         vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
         return Mem(n - 1, amount, coins, dp);
-
-        // Tabulation:
-        // return Tab(coins, n, amount);
-
-        // Space Optimization:
-        // return spaceOptimization(coins, n, amount);
     }
 };
