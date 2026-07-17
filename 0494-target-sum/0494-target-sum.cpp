feec2193ffe6d1fbd9c7190c213ac9371edce0a3
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-07-18 00:04:27 +0530

    Time: 0 ms (100%), Space: 15.6 MB (43.74%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,11 +1,33 @@
 class Solution {
+    int Rec(int ind, int sum, vector<int> &num)
+    {
+        if (ind == 0)
+        {
+            if (sum == 0 && num[0] == 0)
+                return 2;
+
+            if (sum == 0 || num[0] == sum)
+                return 1;
+
+            return 0;
+        }
+
+        int notTake = Rec(ind - 1, sum, num);
+        int take = 0;
+
+        if (num[ind] <= sum)
+            take = Rec(ind - 1, sum - num[ind], num);
+        
+        return (take + notTake);
+    }
+
     int Mem(int ind, int sum, vector<int> &num, vector<vector<int>> &dp)
     {
         // Base case modified from dp17, to handle if (num[ind] == 0) else everything is same.
         if (ind == 0)
         {
             if (sum == 0 && num[0] == 0)
-                return 2;
+                return 2;   // (include + exclude)
 
             if (sum == 0 || num[0] == sum)
                 return 1;
@@ -25,72 +47,16 @@ class Solution {
         return dp[ind][sum] = (notTake + take);
     }
 
-    int Tab(vector<int> &num, int tar)
-    {
-        int n = num.size();
-        vector<vector<int>> dp(n, vector<int>(tar + 1, 0));
-
-        if (num[0] == 0)
-            dp[0][0] = 2;
-        else
-            dp[0][0] = 1;
-
-        if (num[0] != 0 && num[0] <= tar)
-            dp[0][num[0]] = 1;
-
-        for (int i = 1; i < n; i++)
-        {
-            for (int sum = 0; sum <= tar; sum++)
-            {
-                int notTake = dp[i - 1][sum];
-                int take = 0;
-
-                if (num[i] <= sum)
-                    take = dp[i - 1][sum - num[i]];
-
-                dp[i][sum] = (notTake + take);
-            }
-        }
-        return dp[n - 1][tar];
-    }
-
     int findWays(vector<int> &num, int tar)
     {
         int n = num.size();
 
+        // Recursion:
+        // return Rec(n-1, tar, num);
+
         // Memoization:
-        // vector<vector<int>> dp(n, vector<int>(tar + 1, -1));
-        // return Mem(n - 1, tar, num, dp);
-
-        // Tabulation:
-        // return Tab(num, tar);
-
-        // Space Optimization:
-        vector<int> prev(tar + 1, 0), curr(tar + 1, 0);
-
-        if (num[0] == 0)
-            prev[0] = 2;
-        else
-            prev[0] = 1;
-
-        if (num[0] != 0 && num[0] <= tar)
-            prev[num[0]] = 1;
-
-        for (int i = 1; i < n; i++)
-        {
-            for (int sum = 0; sum <= tar; sum++)
-            {
-                int notTake = prev[sum];
-                int take = 0;
-
-                if (num[i] <= sum)
-                    take = prev[sum - num[i]];
-
-                curr[sum] = notTake + take;
-            }
-            prev = curr;
-        }
-        return prev[tar];
+        vector<vector<int>> dp(n, vector<int>(tar + 1, -1));
+        return Mem(n - 1, tar, num, dp);
     }
 
     int countPartition(int n, int d, vector<int> &arr)
