82be1d3d3443f1049832602f93a5c80b223a5a0b
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-07-29 22:43:37 +0530

    Time: 6 ms (68.63%), Space: 14.3 MB (19.83%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,3 +1,5 @@
+// TC: O(NxM)
+// SC: O(NxM)
 class Solution {
     int Rec(int i, int j, string s1, string s2)
     {
@@ -10,80 +12,32 @@ class Solution {
         if (s1[i] == s2[j])
             return Rec(i - 1, j - 1, s1, s2);
 
-        return 1 + min(Rec(i - 1, j, s1, s2), min(Rec(i, j - 1, s1, s2), Rec(i - 1, j - 1, s1, s2)));
+        return 1 + min(Rec(i - 1, j, s1, s2), 
+                    min(Rec(i, j - 1, s1, s2), Rec(i - 1, j - 1, s1, s2)));
     }
 
-    int Mem(int i, int j, string s1, string s2, vector<vector<int>> &dp)
+    int Mem(int i, int j, string &s1, string &s2, vector<vector<int>> &dp)
     {
         if (i < 0)
-            return j + 1;
-
+            return j + 1; // word1 is empty, must INSERT remaining (j+1) characters of word2
+            
         if (j < 0)
-            return i + 1;
+            return i + 1; // word2 is empty, must DELETE remaining (i+1) characters of word1
 
         if (dp[i][j] != -1)
             return dp[i][j];
 
+        // Move both pointers backward without adding to the cost.
         if (s1[i] == s2[j])
             return dp[i][j] = Mem(i - 1, j - 1, s1, s2, dp);
 
-        return dp[i][j] = 1 + min(Mem(i - 1, j, s1, s2, dp),
-                                min(Mem(i, j - 1, s1, s2, dp), Mem(i - 1, j - 1, s1, s2, dp)));
-    }
-
-    int Tab(string s1, string s2)
-    {
-        int n = s1.size(), m = s2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
-        for (int i = 0; i <= n; i++)
-        {
-            dp[i][0] = i;
-        }
-
-        for (int j = 0; j <= m; j++)
-        {
-            dp[0][j] = j;
-        }
-
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-            {
-                if (s1[i - 1] == s2[j - 1])
-                    dp[i][j] = dp[i - 1][j - 1];
-
-                else
-                    dp[i][j] = 1 + min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1]));
-            }
-        }
-        return dp[n][m];
-    }
-
-    int spaceOptimization(string s1, string s2)
-    {
-        int n = s1.size(), m = s2.size();
-        vector<int> prev(m + 1, 0), curr(m + 1, 0);
-
-        for (int j = 0; j <= m; j++)
-        {
-            prev[j] = j;
-        }
-
-        for (int i = 1; i <= n; i++)
-        {
-            curr[0] = i;
-            for (int j = 1; j <= m; j++)
-            {
-                if (s1[i - 1] == s2[j - 1])
-                    curr[j] = prev[j - 1];
-
-                else
-                    curr[j] = 1 + min(prev[j], min(curr[j - 1], prev[j - 1]));
-            }
-            prev = curr;
-        }
-        return prev[m];
+        // IF CHARACTERS DO NOT MATCH: Try all 3 operations and take the minimum cost.
+        int deleteOp  = Mem(i - 1, j, s1, s2, dp);     // Skip s1[i], keep looking for s2[j]
+        int insertOp  = Mem(i, j - 1, s1, s2, dp);     // Match s2[j] with inserted char, keep looking at s1[i]
+        int replaceOp = Mem(i - 1, j - 1, s1, s2, dp); // Substitute s1[i] with s2[j], both are matched
+        
+        // Add 1 to the cost for performing the operation itself
+        return dp[i][j] = 1 + min({deleteOp, insertOp, replaceOp});
     }
 public:
     int minDistance(string str1, string str2) 
@@ -94,13 +48,7 @@ public:
         // return Rec(n - 1, m - 1, str1, str2);
 
         // Memoization:
-        // vector<vector<int>> dp(n, vector<int>(m, -1));
-        // return Mem(n - 1, m - 1, str1, str2, dp);
-
-        // Tabulation:
-        // return Tab(str1, str2);
-
-        // Space Optimization:
-        return spaceOptimization(str1, str2);
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return Mem(n - 1, m - 1, str1, str2, dp);
     }
 };
