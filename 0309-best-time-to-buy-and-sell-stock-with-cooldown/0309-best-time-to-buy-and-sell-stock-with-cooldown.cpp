51c3edcd2465904b7c052e5ce33190e0e44d2a5d
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-08-01 18:40:31 +0530

    Time: 7 ms (7.38%), Space: 16.3 MB (22.71%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -10,7 +10,7 @@ class Solution {
             profit = max(-prices[ind] + Rec(ind + 1, 0, prices, n),
                         0 + Rec(ind + 1, 1, prices, n));
         else
-            profit = max(prices[ind] + Rec(ind + 2, 1, prices, n),
+            profit = max(prices[ind] + Rec(ind + 2, 1, prices, n),  // (ind+2) after selling
                         0 + Rec(ind + 1, 0, prices, n));
 
         return profit;
@@ -35,30 +35,6 @@ class Solution {
 
         return dp[ind][buy] = profit;
     }
-
-    int Tab(vector<int> &prices)
-    {
-        int n = prices.size();
-        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
-
-        dp[n][0] = dp[n][1] = 0;
-
-        for (int ind = n - 1; ind >= 0; ind--)
-        {
-            for (int buy = 0; buy <= 1; buy++)
-            {
-                int profit = 0;
-                if (buy)
-                    profit = max(-prices[ind] + dp[ind + 1][0], 0 + dp[ind + 1][1]);
-                else
-                    profit = max(prices[ind] + dp[ind + 2][1], 0 + dp[ind + 1][0]);
-
-                dp[ind][buy] = profit;
-            }
-        }
-        return dp[0][1];
-    }
-
 public:
     int maxProfit(vector<int>& prices) 
     {
@@ -68,10 +44,7 @@ public:
         // return Rec(0, 1, prices, n);
 
         // Memoization:
-        // vector<vector<int>> dp(n, vector<int>(2, -1));
-        // return Mem(0, 1, prices, n, dp);
-
-        // Tabulation:
-        return Tab(prices);
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return Mem(0, 1, prices, n, dp);
     }
 };
