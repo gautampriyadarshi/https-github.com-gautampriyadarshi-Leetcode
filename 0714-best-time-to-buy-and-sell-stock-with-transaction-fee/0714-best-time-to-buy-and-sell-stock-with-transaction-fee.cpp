9fd1b5a0bc4e14a5b184e397d6dfab0a37862d9e
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-08-01 19:33:48 +0530

    Time: 127 ms (19.61%), Space: 108.5 MB (26.64%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,18 +1,18 @@
 class Solution {
     int Rec(int ind, int buy, vector<int>& prices, int n, int fee) 
     { 
-        if(ind == n) 
-            return 0; 
+        if(ind == n)
+            return 0;
+        
+        int profit = 0;
 
-        int profit = 0; 
-
-        if(buy) 
-            profit = max( -prices[ind] + Rec(ind+1, 0, prices, n, fee), 
-                        0 + Rec(ind+1, 1, prices, n, fee)); 
-        else 
-            profit = max(prices[ind] - fee + Rec(ind+1, 1, prices, n, fee), 
-                        0 + Rec(ind+1, 0, prices, n, fee)); 
-
+        if(buy)
+            profit = max(-prices[ind] + Rec(ind+1, 0, prices, n, fee),
+                        0 + Rec(ind+1, 1, prices, n, fee));
+        else
+            profit = max(prices[ind] - fee + Rec(ind+1, 1, prices, n, fee),
+                        0 + Rec(ind+1, 0, prices, n, fee));
+        
         return profit;
     }
 
@@ -35,53 +35,6 @@ class Solution {
 
         return dp[ind][buy] = profit;
     }
-
-    int Tab(vector<int>& prices, int fee)
-    {
-        int n = prices.size();
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
-
-        dp[n][0] = dp[n][1] = 0;
-
-        for(int ind = n-1; ind >= 0; ind--)
-        {
-            for(int buy = 0; buy <= 1; buy++)
-            {
-                int profit = 0; 
-                if(buy) 
-                    profit = max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1]); 
-                else 
-                    profit = max(prices[ind] - fee + dp[ind+1][1], 0 + dp[ind+1][0]); 
-
-                dp[ind][buy] = profit;
-            }
-        }
-        return dp[0][1];
-    }
-
-    int spaceOptimization(vector<int>& prices, int fee)
-    {
-        int n = prices.size();
-        vector<int>ahead(2, 0), curr(2, 0);
-
-        ahead[0] = ahead[1] = 0;
-
-        for(int ind = n-1; ind >= 0; ind--)
-        {
-            for(int buy = 0; buy <= 1; buy++)
-            {
-                int profit = 0; 
-                if(buy) 
-                    profit = max(-prices[ind] + ahead[0], 0 + ahead[1]); 
-                else 
-                    profit = max(prices[ind] - fee + ahead[1], 0 + ahead[0]); 
-
-                curr[buy] = profit;
-            }
-            ahead = curr;
-        }
-        return ahead[1];
-    }
 public:
     int maxProfit(vector<int>& prices, int fee) 
     {
@@ -91,13 +44,7 @@ public:
         // return Rec(0, 1, prices, n, fee);
 
         // Memoization:
-        // vector<vector<int>> dp(n, vector<int>(2, -1));
-        // return Mem(0, 1, prices, n, fee, dp);
-
-        // Tabulation:
-        // return Tab(prices, fee);
-
-        // Space Optimization:
-        return spaceOptimization(prices, fee);
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return Mem(0, 1, prices, n, fee, dp);
     }
 };
