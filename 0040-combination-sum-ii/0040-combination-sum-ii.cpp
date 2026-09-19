3026685eda4df00d13c90ecd769dd8990cb358a7
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-09-19 19:14:44 +0530

    Time: 0 ms (100%), Space: 14 MB (63.55%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,82 +1,35 @@
-/* Giving correct output of all cases but memory limit exceeded:
 class Solution {
-    bool totalSum(vector<int> &subset, int target)
+    void solve(int ind, vector<int>& candidates, int target, vector<int>& help, vector<vector<int>>& ans) 
     {
-        int x = 0;
-        for(int i = 0; i < subset.size(); i++)
+        if (target == 0) 
         {
-            x += subset[i];
-        }
-        
-        if(x == target)
-            return true;
-        
-        return false;
-    }
-    void findSubset(int i, int target, set<vector<int>> &st, vector<int>& candidates, vector<int> subset)
-    {
-        if(i == candidates.size())
-        {
-            if(totalSum(subset, target))
-            {
-                sort(subset.begin(), subset.end());
-                st.insert(subset);
-            }
-            return;
-        }
-        
-        subset.push_back(candidates[i]);
-        findSubset(i+1, target, st, candidates, subset);
-        subset.pop_back();
-        findSubset(i+1, target, st, candidates, subset);
-    }   
-public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) 
-    {
-        vector<int> subset;
-        set<vector<int>> st;
-        findSubset(0, target, st, candidates, subset);
-
-        vector<vector<int>> ans;
-        for(auto x: st)
-        {
-            ans.push_back(x);
-        }
-        return ans;
-    }
-};  */
-
-class Solution {
-    void backtrack(vector<int> &candidates, vector<int> &output,vector<vector<int>> &ans ,int index, int target)
-    {
-        if(target == 0)
-        {
-            ans.push_back(output) ;
+            ans.push_back(help);
             return;
         }
 
-        if(target < 0 || index >= candidates.size())
-            return;
-
-        for(int i = index ; i < candidates.size() ; i++)
+        for (int i = ind; i < candidates.size(); i++) 
         {
-            if(i > index && candidates[i] == candidates[i-1])
+            // Skip duplicates at the same level of the recursion tree
+            if (i > ind && candidates[i] == candidates[i - 1]) 
                 continue;
             
-            output.push_back(candidates[i]);
-            backtrack(candidates, output, ans, i+1, target-candidates[i]);
-            output.pop_back();
+            // Optimization: Since array is sorted, if current element exceeds target, further elements will too
+            if (candidates[i] > target) 
+                break;
+
+            help.push_back(candidates[i]);
+            solve(i + 1, candidates, target - candidates[i], help, ans);
+            help.pop_back();
         }
     }
-
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) 
     {
-        sort(candidates.begin(), candidates.end());
-        vector<vector<int>>ans ;
-        vector<int>output ;
-        int index = 0 ;
-        backtrack(candidates, output, ans, index, target);
-        return ans ;
+        sort(candidates.begin(), candidates.end()); 
+        vector<vector<int>> ans;
+        vector<int> help;
+        solve(0, candidates, target, help, ans);
+
+        return ans;
     }
 };
