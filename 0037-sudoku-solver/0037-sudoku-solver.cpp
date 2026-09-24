44d42a4280e9242726f5901ba63325d18e034793
Gautam Priyadarshi <104095324+gautampriyadarshi@users.noreply.github.com>
2026-09-24 19:41:59 +0530

    Time: 47 ms (97.48%), Space: 8.8 MB (89.72%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,64 +1,68 @@
 class Solution {
-    bool isSafe(vector<vector<char>>& board, int row, int col, char value)
+    // Arrays to track if a number (1-9) is already used in a row, col, or 3x3 box
+    bool rowCheck[9][10] = {false};
+    bool colCheck[9][10] = {false};
+    bool boxCheck[9][10] = {false};
+    
+    // Store coordinates of all empty cells to avoid re-scanning the board
+    vector<pair<int, int>> emptyCells;
+
+    bool solve(vector<vector<char>>& board, int index) 
     {
-        int n = board.size();
+        // Base case: If we've filled all empty cells, the Sudoku is solved
+        if (index == emptyCells.size())
+            return true;
 
-        for(int i = 0; i < n; i++)
+        int r = emptyCells[index].first;
+        int c = emptyCells[index].second;
+        int boxId = (r / 3) * 3 + (c / 3); // Formula to map 2D coordinates to 0-8 box index
+
+        for (int val = 1; val <= 9; val++) 
         {
-            if(board[row][i] == value)
-                return false;
-
-            if(board[i][col] == value)
-                return false;
-            
-            if(board[3*(row/3) + (i/3)][3*(col/3) + (i%3)] == value)
-                return false;
-        }
-        return true;
-    }
-
-    bool solve(vector<vector<char>>& board)
-    {
-        int n = board.size();
-
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < n; j++)
+            // O(1) check to see if the number is safe to place
+            if (!rowCheck[r][val] && !colCheck[c][val] && !boxCheck[boxId][val]) 
             {
-                if(board[i][j] == '.')
-                {
-                    for(char value = '1'; value <= '9'; value++)
-                    {
-                        if(isSafe(board, i, j, value))
-                        {
-                            board[i][j]= value;
+                // Place the digit and update tracking arrays
+                board[r][c] = val + '0';
+                rowCheck[r][val] = colCheck[c][val] = boxCheck[boxId][val] = true;
 
-                            // recursive call
-                            bool nextsolution = solve(board);
-
-                            // here we are returning true because the question 
-                            // clearly states that there will be only one answer.
-                            if(nextsolution == true)  
-                                return true;
-                                
-                            // backtrack
-                            else    
-                                board[i][j]='.';
-                        }
-                    }
-                    // if we were not able to fill the current cell by any of digit 
-                    // 1-9 that means we put wrong value in previous cell, 
-                    // so returning false to go back 
-                    return false;
+                // Recurse to the next empty cell
+                if (solve(board, index + 1)) {
+                    return true;
                 }
+
+                // Backtrack: Remove the digit and reset tracking arrays
+                board[r][c] = '.';
+                rowCheck[r][val] = colCheck[c][val] = boxCheck[boxId][val] = false;
             }
         }
-        // if we reach this line that means we ahve filled the sudoku correctly.
-        return true;
+        return false;
     }
+
 public:
     void solveSudoku(vector<vector<char>>& board) 
     {
-        solve(board);
+        // Step 1: Initialize tracking arrays and find all empty cells just ONCE
+        for (int i = 0; i < 9; i++) 
+        {
+            for (int j = 0; j < 9; j++) 
+            {
+                if (board[i][j] == '.') 
+                    emptyCells.push_back({i, j});
+                else 
+                {
+                    int val = board[i][j] - '0';
+                    int boxId = (i / 3) * 3 + (j / 3);
+                    
+                    // Mark pre-existing numbers as used
+                    rowCheck[i][val] = true;
+                    colCheck[j][val] = true;
+                    boxCheck[boxId][val] = true;
+                }
+            }
+        }
+
+        // Step 2: Start backtracking using the list of empty cells
+        solve(board, 0);
     }
 };
